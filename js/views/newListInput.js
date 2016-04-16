74dbe4ca0647332889323f8fe4be1481bcfdb63e
Gabriel Mojica <niko.mojica@live.com>
2016-04-16 23:22:10 +1000

    refactor: Rename 'input' to 'element'

--- js/views/newListInput.js
+++ js/views/newListInput.js
@@ -1,27 +1,27 @@
 var NewListInput = function() {
-    this.input = $('<input id="newlist-input" class="hidden">');
+    this.element = $('<input id="newlist-input" class="hidden">');
     this.database = new Database();
 };
 
 NewListInput.prototype.create = function() {
-    this.input.val('');
-    this.input.toggleClass('hidden');
-    this.input.focus();
+    this.element.val('');
+    this.element.toggleClass('hidden');
+    this.element.focus();
 };
 
 NewListInput.prototype.render = function() {
     var self = this;
-    self.input.keyup(function(event) {
+    self.element.keyup(function(event) {
         var value = self.input.val();
         if (event.keyCode !== 13 || value === '') return;
 
         self.database.createList(value);
-        self.input.addClass('hidden');
+        self.element.addClass('hidden');
     });
 
-    self.input.focusout(function() {
-        self.input.addClass('hidden');
+    self.element.focusout(function() {
+        self.element.addClass('hidden');
     });
 
-    $('#newlist-container').append(self.input);
+    $('#newlist-container').append(self.element);
 };

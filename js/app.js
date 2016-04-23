867a1a98aa17c561e2ef490eda28a9e0b1254511
Gabriel Mojica <niko.mojica@live.com>
2016-04-23 16:38:43 +1000

    feat: Create navbar layout

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var App = function() {
     this.hotkeys = new Hotkeys();
     this.inputListName = new InputListName();
+    this.navbar = new Navbar();
 };
 
 App.prototype.initializeControls = function() {
     var self = this;
-
     self.hotkeys.listen(function(key) {
         var inputVisible = self.checkListNameInput(key);
         if (inputVisible) return;
---
+++ js/navbar.js
@@ -0,0 +1,4 @@
+var Navbar = function() {
+    this.element = $('#navbar');
+};
+
